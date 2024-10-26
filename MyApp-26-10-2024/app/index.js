7b2ac2e05efcbef1823760c58d9beb9a6226188d
xVanze <ductruong2403@gmail.com>
2024-10-26 18:13:37 +0700

    26/10

---
+++ MyApp-26-10-2024/app/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import FriendListScreen from '../components/FriendListScreen'
+import FlexBoxDemo from '../components/FlexBoxDemo'
+
+
+const index = () => {
+  return (
+    <FlexBoxDemo></FlexBoxDemo>
+  )
+}
+
+
+export default index
