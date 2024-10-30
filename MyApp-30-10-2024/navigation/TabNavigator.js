76fdd570b1d8ba9f87163c16a2595cdeb13e20ac
xVanze <ductruong2403@gmail.com>
2024-10-30 18:06:58 +0700

    30/10/2024

---
+++ MyApp-30-10-2024/navigation/TabNavigator.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../components/HomeScreen';
+import SettingsScreen from '../components/ProfileScreen';
+import ProfileScreen from '../components/SettingsScreen';
+import { Ionicons } from '@expo/vector-icons'; // Sử dụng các biểu tượng từ thư viện Ionicons
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#4CAF50',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
